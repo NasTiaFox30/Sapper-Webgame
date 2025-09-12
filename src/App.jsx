4d48a3faa49f937224d8fc77914e961479919534
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 15:15:46 +0200

    handleGameOver and handleNewGame fucntion in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import './css/App.css';
 export default function App() {
   const [gameStatus, setGameStatus] = useState('playing');
   const [resetKey, setResetKey] = useState(0);
+
+  const handleGameOver = (isWin) => {
+    setGameStatus(isWin ? 'win' : 'lose');
+  };
+
+  const handleNewGame = () => {
+    setGameStatus('playing');
+    setResetKey(prevKey => prevKey + 1);
+  };
+
   return (
     <> App
     </>
