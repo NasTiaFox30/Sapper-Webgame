5f71a55b02e272a93a16b2f33ee073ed1848f7f5
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 14:35:03 +0200

    added th elast of logic of cell

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -14,5 +14,23 @@ export default function Cell({ cell, onClick, onContextMenu }) {
     }
   };
 
-  
+  const getCellContent = () => {
+    if (cell.isRevealed) {
+      if (cell.isMine) {
+        return '💣';
+      }
+      return cell.mineCount > 0 ? cell.mineCount : ''; // Number or Empty
+    }
+    return cell.isFlagged ? '🚩' : ''; // Flag or Empty
+  };
+
+  return (
+    <div
+      className={`cell ${cell.isRevealed ? 'revealed' : ''} ${cell.isFlagged ? 'flagged' : ''}`}
+      onClick={handleClick}
+      onContextMenu={handleRightClick}
+    >
+      {getCellContent()}
+    </div>
+  );
 };
