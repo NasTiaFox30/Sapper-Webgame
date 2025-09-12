af60ad2dbe1df804c6358243676b52cedf6a8c16
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 14:36:51 +0200

    created Board.jsx

---
+++ src/components/Board.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+import Cell from './Cell';
+import '../css/Board.css';
+
+export default function Board({ rows, cols, mines, onGameOver, onWin, onReset }) {
+  
+};
