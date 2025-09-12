a19f1f1c8b8082f5d73ed71c937d4df877c66da9
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 14:39:55 +0200

    added useStates (default paraments of game)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,5 +3,9 @@ import Cell from './Cell';
 import '../css/Board.css';
 
 export default function Board({ rows, cols, mines, onGameOver, onWin, onReset }) {
+  const [board, setBoard] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [flags, setFlags] = useState(0);
+
   
 };
