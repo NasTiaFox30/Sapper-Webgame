9fb572032877be2320cf0d92b015695afdfcff74
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 15:13:26 +0200

    creted useStates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Board from './components/Board';
 import './css/App.css';
 
 export default function App() {
+  const [gameStatus, setGameStatus] = useState('playing');
+  const [resetKey, setResetKey] = useState(0);
   return (
     <> App
     </>
