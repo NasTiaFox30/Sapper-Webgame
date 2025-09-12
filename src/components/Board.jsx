0f9034d5917a363f07d5f2dc5b6a6b1554f35539
Nastia Bzova <bzovanastia@gmail.com>
2025-09-12 14:49:30 +0200

    added new calculateNumber - show the right countof bombs near cell

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -40,4 +40,33 @@ export default function Board({ rows, cols, mines, onGameOver, onWin, onReset })
         return newBoard;
     };
 
+    const calculateNumbers = (board) => {
+        const newBoard = JSON.parse(JSON.stringify(board));
+
+        for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (!newBoard[row][col].isMine) {
+            let count = 0;
+            
+            directions.forEach(([dx, dy]) => {
+                const newRow = row + dx;
+                const newCol = col + dy;
+                
+                // check if cell exists
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                // if mine - add +1
+                if (newBoard[newRow][newCol].isMine) {
+                    count++;
+                }
+                }
+            });
+            
+            newBoard[row][col].mineCount = count;
+            }
+        }
+        }
+        
+        return newBoard;
+    };
+
 };
