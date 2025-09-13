c35eed4e6ec2e81d571d2945aceb6272be813f98
Nastia Bzova <bzovanastia@gmail.com>
2025-09-13 02:51:54 +0200

    creted themesStorage - for different themes

---
+++ src/themes/themesStorage.js
@@ -0,0 +1,44 @@
+// Themes:
+export const themesConfig = {
+  cybersweeper: {
+    name: 'CyberSweeper',
+    preview: '👾',
+    assets: {
+      cell: import('../assets/sprites/cybersweeper/cell.png'),
+      flag: import('../assets/sprites/cybersweeper/flag.gif'),
+      bomb: import('../assets/sprites/cybersweeper/bomb.gif'),
+      reset: import('../assets/sprites/cybersweeper/reset.png'),
+    },
+    styles: 'cybersweeper' // css
+  },
+  meowmine: {
+    name: 'MeowMine',
+    preview: '🐭',
+    assets: {
+      cell: import('../assets/sprites/meowmine/cell.png'),
+      flag: import('../assets/sprites/meowmine/flag.png'),
+      bomb: import('../assets/sprites/meowmine/bomb.png'),
+      reset: import('../assets/sprites/meowmine/reset.png'),
+    },
+    styles: 'meowmine' //css
+  },
+  classic: {
+    name: 'Classic',
+    preview: '🎮',
+    assets: {
+      // used classic icons
+    },
+    styles: 'classic' // css
+  }
+};
+
+export const getThemeAssets = async (themeName) => {
+  const theme = themesConfig[themeName];
+  const assets = {};
+  
+  for (const [key, value] of Object.entries(theme.assets)) {
+    assets[key] = (await value).default;
+  }
+  
+  return assets;
+};
